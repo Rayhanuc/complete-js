af847d6566661efd7c5b62218e2e0a41e23814ea
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-05-31 16:35:36 +0600

    17. Truthy and Falsy Values and Equality Operators
    17. Truthy and Falsy Values and Equality Operators

---
+++ 17/script.js
@@ -0,0 +1,28 @@
+/************************** 
+Truthy and Falsy Values and Equality Operators
+***************************/
+
+
+// falsy values: undefined, null, 0, '', NaN
+// truthy values: NOT falsy values
+
+var height;
+
+// height = 0;
+height = 23;
+
+if (height || height === 0) {
+	console.log('Variable is defined');
+} else {
+	console.log('Variable has NOT been defined');
+}
+
+
+
+// Equality operator
+
+if (height === '23') {
+	console.log('The == operator does type coercion!');
+}else {
+	console.log('Both value are not same.');
+}
