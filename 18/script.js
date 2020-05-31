8dbabd2b0ede905cd3c23b7dd6cbd6e083fae89f
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-05-31 20:24:43 +0600

    18. Coding Challenge 2
    18. Coding Challenge 2

---
+++ 18/script.js
@@ -0,0 +1,20 @@
+/************************** 
+CODING CHALLENGE 2
+***************************/
+
+
+/*
+John and Mike both play basketbal in different teams. In the latest 3 games, John's tem scord 89, 120 and 103 points, white Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team
+2. Decide which teams wins in avarage (heighest avarage score), and print the winner to the console.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same avarage score)
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the avarage winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, It's no problem :)
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+
+Good Lick
+*/
+
+
+
