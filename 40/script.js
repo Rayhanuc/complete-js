257c597954d7404177998ea58681ccd348c9d842
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-06-24 20:03:00 +0600

    40. Scoping and the Scope Chain
    40. Scoping and the Scope Chain

---
+++ 40/script.js
@@ -0,0 +1,27 @@
+////////////////////////
+// Lecture: Scoping
+
+// First scoping example
+
+var a = 'Hello';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+        third();
+    }
+}
+
+/* function third() {
+    var d = 'John';
+    console.log(c);
+} */
+function third() {
+    var d = ' John';
+    console.log(a + d);
+}
